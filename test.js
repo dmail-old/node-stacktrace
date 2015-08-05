7c3b47eb9a970f99916594af252f52c029b0aa40
Damien Maillard <dmaillard06@gmail.com>
2015-08-05 18:14:01 +0200

    update test

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var CallSite = require('./call-site');
 
+function parseLine(line){
+	return CallSite.parseLine(line);
+}
+
 exports['at repl:1:1'] = function(test){
-	test.compare(CallSite.parseLine('at repl:1:1'), {
-		functionName: 'repl',
-		line: 1,
-		column: 1
-	});
+	test.deepEqual(parseLine('at repl:1:1'), {functionName: 'repl', line: 1, column: 1});
 };
 
